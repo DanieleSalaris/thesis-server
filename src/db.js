f37617f8d6442817c2a4ce15f072190dbef95993
daniele <dnl.salaris@gmail.com>
2021-10-26 17:36:05 +0200

    atlas url

--- src/db.js
+++ src/db.js
@@ -4,8 +4,12 @@ const DB_URL = process.env.DATABASE_URL || 'mongodb://localhost'
 const DB_NAME = process.env.DB_NAME || 'tesi_db'
 const DB_USER = process.env.DATABASE_USER || 'root'
 const DB_PASSWORD = process.env.DATABASE_PASSWORD || 'password'
+const DB_FULL_URL = process.env.DATABASE_FULL_URL
 
 async function connectDb(dbName=DB_NAME, dbUrl=DB_URL, user=DB_USER, password=DB_PASSWORD) {
+  if (DB_FULL_URL) {
+    return mongoose.connect(DB_FULL_URL)
+  }
   return mongoose.connect(`${dbUrl}/${dbName}`, {
     auth: {
       authSource: 'admin',
